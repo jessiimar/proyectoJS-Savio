69b012878c8c7e880be36abebb82413d77b05d7c
jessica <jessiimar.06@gmailo.com>
2023-01-10 21:19:01 -0300

    preentrega1-savio

---
+++ pre-entrega1JS/index.js
@@ -0,0 +1,28 @@
+//pruyecto jessica savio
+const categoria1 = 'bebe'
+const categoria2 = 'niños'
+const categoria3 = 'adolescentes'
+
+
+let categoria = prompt('bienvenido/a a GnG-indumentaria. que categoria buscas?')
+
+
+if (categoria == categoria1){
+    alert('haz ingresado a la seccion de bebes')
+}else if(categoria  == categoria2){
+
+    alert('haz ingresado a la seccion de niños')
+}else if(categoria ==categoria3){
+    alert('haz ingresado a la seccion de adolescentes')
+} else{
+    alert('lo siento, pero esa palbra clave no esta en nuestro registo')
+};
+
+
+
+
+for (i = 0; i < 2; i++){
+    let niños = 20;
+    console.log (niños <= 2)
+    alert('aprovecha el  50% de descuento en la segunda unidad!');
+}
